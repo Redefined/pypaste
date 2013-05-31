2a347519d9d7bb8e83ea56155ef5fa700d65fb18
ares666 <mateescu.victor@gmail.com>
2013-05-31 17:21:55 +0200

    bullshit bugs dammit, won't even go past syntax crap

--- p.py
+++ p.py
@@ -46,7 +46,7 @@ def checkconfigfile():
     if args.config != "":
         path = args.config
     else:
-        path = "pypaste.cfg"
+        path = "pypaste.cfg"  # defaults to this file in the same folder as the script
     try:
         tmpfile = open(path)
     except IOError, e:
@@ -59,31 +59,90 @@ def checkconfigfile():
         return tmpfile
 
 
-def checkconfig_value(val):
-    global config
+def init_config():
+    global config, args
     config = ConfigParser.ConfigParser()
     config.read(args.config)
+    return config
+
+
+def checkconfig_value(val, config):
     debug("Testing configuration file critical keys...for key: " + val)
     found = False
     for section in config.sections():
         if config.has_option(section, val):
             found = True
     if found == False:
-        crit("one of the critical keys was not present in the config file")
-        debug("    %s value not found" % val)
+        crit("one of the critical keys was not present in the config file: %s" % val)
+        debug("\t%s value not found" % val)
         return False
-        sys.exit(1)
+        sys.exit(2)
     else:
         return True
 
 
+def checkconfig_siteurls(site, config):
+    url_list = []
+    url_list.append(config.get(site,'latest_url'))
+    url_list.append(config.get(site,'trends_now_url'))
+    url_list.append(config.get(site,'trends_week_url'))
+    url_list.append(config.get(site,'trends_month_url'))
+    url_list.append(config.get(site,'trends_year_url'))
+    url_list.append(config.get(site,'trends_all_url'))
+    return url_list
+
+
+def checkconfig_searchterms(config):
+    searchterm_list = []
+    searchterm_list.append(config.get('search', 'term1'))
+    searchterm_list.append(config.get('search', 'term2'))
+    searchterm_list.append(config.get('search', 'term3'))
+    searchterm_list.append(config.get('search', 'term4'))
+    searchterm_list.append(config.get('search', 'term5'))
+    searchterm_list.append(config.get('search', 'term6')) 
+    searchterm_list.append(config.get('search', 'term7'))
+    searchterm_list.append(config.get('search', 'term8'))
+    searchterm_list.append(config.get('search', 'term9'))
+    searchterm_list.append(config.get('search', 'term10'))
+    return searchterm_list
+
 ###
-#
+# SITE-SPECIFIC FUNCTIONS
 ###
+
+def search_raw(needle, haystack, linkid):
+    if re.search(r'' + needle, haystack):
+        info('\t *** FOUND \'' + needle + '\' in link ' + linkid + '\n')
+
+
+def read_site(url, matchstr, paste_ids_position):
+    try:
+        lnk = urllib.urlopen(url)
+        htm = lnk.read()
+        lnk.close()
+        htm_lines = htm.split('\n')
+        for line in htm_lines:
+            if re.search(matchstr, line):
+                link_id = line[paste_ids_position]
+                debug('Link_id: ' + link_id)
+                if re.search('pastebin', url):
+                    url_2 = urllib.urlopen("http://pastebin.com/raw.php?i=" + link_id)
+                else:
+                    url_2 = urllib.urlopen("http://pastie.org/pastes/" + link_id + "/text")
+                raw_paste = url_2.read()
+                url_2.close()
+    except(IOError):
+        print("can\'t connect to url: %s" % url)
+    except:
+        print('fatal exception... bailing out')
+    return (raw_paste, link_id)
+
+
 
 
 if __name__ == "__main__":
-    ConfigParser.RawConfigParser()
+    global args, regex_weblink_string, paste_ids_position
+    config = init_config()
     argparser = argparse.ArgumentParser(description='Pastebin&Co. string checker script')
     argparser.add_argument('-c', '--config', type=str, default='pypaste.cfg', help='/path/to/config.cfg ; it defaults to \'pypaste.cfg\' in the same path as the script')
     argparser.add_argument('-d', '--debug', default=False, action='store_true', help='Debug mode(1=True, 0=False), defaults to False')
@@ -91,43 +150,31 @@ if __name__ == "__main__":
     args = argparser.parse_args()
     if not checkconfigfile():
         exit(2)
+    cfg = init_config()
 
     # User-defined variables, to be moved in config file under [system] section, and urls under [pastebin.com] and so on sections.
-    error_on_cl_args = "Please provide a single regex search via the command line"   # Error to display if improper command line arguments are provided
     archive_url = "http://pastebin.com/archive"    # whether you wanna scan the normal archive (you're lucky to grab the latest 10 minutes worth of pasties)
     trending_url = "http://pastebin.com/trends"    # best to scan is the trending pasties page, since you can see the most 'popular' stuff which is more important anyway.
 
-    if args.expression and not args.config:  # if you only specify -searchterm and don't specify explicitly a config file it means you want only that term searched, ignoring rest of terms in config.
-        search_term = args.expression
+    if args.searchterm and not args.config:  # if you only specify -searchterm and don't specify explicitly a config file it means you want only that term searched, ignoring rest of terms in config.
+        search_term = args.searchterm
+        cliterm = True  # using this to let the logic know to only compare this to raw text, and not cycle through a bunch of terms expected from config.
 
-    dryrun = True
+    checkconfig_value('dryrun', config)
+    checkconfig_value('debug', config)
+    if not args.searchterm and not checkconfig_value('term1'): #  we should have at least one search term either coming from the config file or the command line, otherwise bail with crit exit(2)
+        crit('no searchterms specified')
+    else:
+        info('searchterms are:')
+    dryrun = config.get('system','dryrun')
+    print dryrun
     counter = 0
 
-    #Open the recently posted pastes page
+    # main loop basically
     if not dryrun:
-        try:
-            url = urllib.urlopen(trending_url)
-            html = url.read()
-            url.close()
-            html_lines = html.split('\n')
-            for line in html_lines:
-                if counter < 10:
-                    if re.search(r'<td><img src=\"/i/t.gif\"  class=\"i_p0\" alt=\"\" border=\"0\" /><a href=\"/[0-9a-zA-Z]{8}">.*</a></td>', line):
-                        link_id = line[72:80]
-                        print "Link id: " + link_id
-    
-                        #Begin loading of raw paste text
-                        url_2 = urllib.urlopen("http://pastebin.com/raw.php?i=" + link_id)
-                        raw_text = url_2.read()
-                        url_2.close()
-                        print "\tLength of raw text in this pastie is: %d" % len(raw_text)
-    
-                        #if search_term in raw_text:
-                        if re.search(r'' + search_term, raw_text):
-                            print "\n !!!FOUND a reference to '" + search_term + "' in http://pastebin.com/raw.php?i=" + link_id + "\n"
-    
-                        counter += 1
-        except(IOError):
-            print "can't connect, haz you got networkz?"
-        except:
-            print "Fatal exception. Bailing out..."
+        for url in checkconfig_siteurls('pastebin'):
+            paste_ids_position = config.get('pastebin','paste_ids_position')
+            matchstrings = '\'' + config.get('pastebin','paste_ids_match_string') + '\''  # shit like '<td><img src=\"/i/t.gif\"  class=\"i_p0\" alt=\"\" border=\"0\" /><a href=\"/[0-9a-zA-Z]{8}">.*</a></td>' from config, site dependent, obviously
+            raw, linkid = read_site(url, matchstrings)  
+            for item in searchterms:
+                search_raw(item, raw, linkid)
